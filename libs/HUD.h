7dda8cc0e8dc64fd12f79a669b6f75ee05c3a223
Taylor Horta Domingos Viana <taylorhorta@gmail.com>
2025-10-24 22:56:25 -0300

    organizacao das funcoes que eu fiz

--- libs/HUD.h
+++ libs/HUD.h
@@ -6,3 +6,33 @@ void drawHUD(int score, int totalPellets){
     DrawText(TextFormat("Score: %d", score), 10, 810, 20, WHITE);
     DrawText(TextFormat("pellets: %d", totalPellets), 150, 810, 20, WHITE);
 }
+
+void gameOver(void)
+{
+    int tam_over = 80;
+    int tam_resto = 20;
+    char texto_over[] = {"FIM DE JOGO"};
+    char texto_menu[] = {"V para retornar ao MENU"};
+    char texto_sair[] = {"ESC para sair do jogo"};
+    bool game_over = true;
+    while(game_over == true)
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("FIM DE JOGO", (LARGURA - MeasureText(texto_over, tam_over))/2, ALTURA/2 - tam_over/2, 80, RED);
+        DrawText("V para retornar ao MENU", (LARGURA - MeasureText(texto_menu, tam_resto))/2, ALTURA/2 + tam_resto*3, tam_resto, YELLOW);
+        DrawText("ESC para sair do jogo", (LARGURA - MeasureText(texto_sair, tam_resto))/2, ALTURA/2 + tam_resto*5, tam_resto, YELLOW);
+        EndDrawing();
+
+        if(IsKeyPressed(KEY_V))
+        {
+            //voltar ao menu de alguma forma
+            game_over = false;
+        }
+        if(IsKeyPressed(KEY_ESCAPE))
+        {
+            CloseWindow();
+            exit(0);
+        }
+    }
+}
---
+++ libs/system.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <stdio.h>
+#include "raylib.h"
+#include <stdlib.h>
+
