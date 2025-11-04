8dc5ee6ed751fa09f351394e09c5b0a87e11a165
Guilherme <guilhermebritodemorais06@gmail.com>
2025-11-04 14:05:21 -0300

    Add menu options and update menu logic

--- libs/menu.h
+++ libs/menu.h
@@ -1,5 +1,39 @@
 
+
+enum menuOptions{
+  BACK,
+  SAVE,
+  LOAD,
+  QUIT
+};
+
 void menuLogic(){
+  DrawRectangle(0, 0, LARGURA, ALTURA, Fade(BLACK, 0.8f));
+  DrawText("PAUSE", 10, 10, 40, YELLOW);
+  DrawText("Aperte V para voltar", 10, 45, 20, YELLOW);
+  DrawText("N: Novo Jogo", 10, 100, 20, PINK);
+  DrawText("C: Carregar", 10, 150, 20, ORANGE);
+  DrawText("S: Salvar", 10, 200, 20, CYAN);
+  DrawText("Q: Sair", 10, 250, 20, RED);
+}
+
+void isHighlighted(){
+  
+}
+
+void menuInputs(){
+  if(IsKeyPressed(KEY_V)){
+    state_atual = GAMEPLAY;
+  }
+  if(IsKeyPressed(KEY_N) ||){
+    //resetGameState();
+  }
+  
+  if(IsKeyPressed(KEY_Q)){
+    CloseWindow();
+    freeDiddy(grid_mapa);
+    return 0;
+  }
 
 }
 
