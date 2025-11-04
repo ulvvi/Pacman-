a6ba4a00f2b0d99e9b0f1d7d2243697e40f833cf
Guilherme <guilhermebritodemorais06@gmail.com>
2025-11-04 13:59:12 -0300

    Refactor main function and remove unused code

--- main.c
+++ main.c
@@ -148,26 +148,7 @@ int main(void)
             case PAUSE:
                 //logica do menu
                 switchMusic(MENU, stems);
-
-                DrawRectangle(0, 0, LARGURA, ALTURA, Fade(BLACK, 0.8f));
-                DrawText("PAUSE", 10, 10, 40, YELLOW);
-                DrawText("Aperte V para voltar", 10, 45, 20, YELLOW);
-                DrawText("N: Novo Jogo", 10, 100, 20, PINK);
-                DrawText("C: Carregar", 10, 150, 20, ORANGE);
-                DrawText("S: Salvar", 10, 200, 20, CYAN);
-                DrawText("Q: Sair", 10, 250, 20, RED);
-                if(IsKeyPressed(KEY_V)){
-                    state_atual = GAMEPLAY;
-                }
-                if(IsKeyPressed(KEY_N)){
-                    //resetGameState();
-                }
-
-                if(IsKeyPressed(KEY_Q)){
-                    CloseWindow();
-                    freeDiddy(grid_mapa);
-                    return 0;
-                }
+                menuLogic();
             break;
 
             case CUT_IN:
@@ -197,6 +178,7 @@ int main(void)
     freeMatrizAux(matriz_auxiliar);
     return 0;
 }
+
 
 
 
