f072f83154632bf3db2ad04d491a7da328085d8f
Guilherme <guilhermebritodemorais06@gmail.com>
2025-11-04 12:34:17 -0300

    Implement dynamic music functions
    Added functions for dynamic music management including playMusic and switchMusic.

--- libs/music.h
+++ libs/music.h
@@ -1,5 +1,21 @@
 //funçôes da musica
 
+/*
+    A ideia geral é ter um vetor com as 4 musicas, que serão trocadas dinamicamente de acordo com o jogo
+*/
+
+// para serem passados para o switchMusic
+enum {
+    NORMAL,
+    MENU,
+    POWER,
+    JACKPOT
+}
+
+//volume normal
+float normalVolume = 1.00f
+
+//recebe o conjunto de musicas e inicia todas ao mesmo tempo para ser dinamico
 void playMusic(Music* stems){
     for(int i = 0; i < 3; i++){
         PlayMusicStream(stems[i]);
@@ -34,4 +50,18 @@ void stopAllMusic(Music* stems){
         StopMusicStream(stems[i]);
     }
     return;
+}
+
+/*
+    Função recebe um estado que foi dado no enum acima e troca a musica;
+*/
+void switchMusic(int state, Music* stems){
+    for(int i = 0; i < 3; i++){
+         if(i == state){
+             SetMusicVolume(stems[i], normalVolume);
+         }
+         else{
+             SetMusicVolume(stems[i], 0.00f);
+         }   
+    }
 }
