859867ef1cfaba85ee96f20d517f14367458cf34
Guilherme <guilhermebritodemorais06@gmail.com>
2025-11-04 14:29:41 -0300

    Update menu.h

--- libs/menu.h
+++ libs/menu.h
@@ -8,7 +8,13 @@ enum menuOptions{
   QUIT
 };
 
-void menuLogic(){
+void menuLogic(int* index){
+  drawMenu(index);
+  printf("%d", *index);
+  menuInputs(index);
+}
+
+drawMenu(int* index){
   DrawRectangle(0, 0, LARGURA, ALTURA, Fade(BLACK, 0.8f));
   DrawText("PAUSE", 10, 10, 40, YELLOW);
   DrawText("Aperte V para voltar", 10, 45, 20, YELLOW);
@@ -16,27 +22,27 @@ void menuLogic(){
   DrawText("C: Carregar", 10, 150, 20, ORANGE);
   DrawText("S: Salvar", 10, 200, 20, CYAN);
   DrawText("Q: Sair", 10, 250, 20, RED);
-  menuInputs();
 }
 
 currentChosen(cur){
   if(IsKeyPressed(KEY_ENTER)){
-    return cur
+    return cur;
   }
   else
   {
-    return -1
+    return -1;
   }
 }
 
 void menuInputs(int* index){
-  if(IsKeyPressed(KEY_V) || currentChosen = BACK){
+  if(IsKeyPressed(KEY_V) || currentChosen(*index) = BACK){
+    *index = 0;
     state_atual = GAMEPLAY;
   }
-  if(IsKeyPressed(KEY_N) || currentChosen == NEW){
+  if(IsKeyPressed(KEY_N) || currentChosen(*index) == NEW){
     //resetGameState();
   }
-  if(IsKeyPressed(KEY_Q) || currentChosen == QUIT){
+  if(IsKeyPressed(KEY_Q) || currentChosen(*index) == QUIT){
     CloseWindow();
     freeDiddy(grid_mapa);
     return 0;
