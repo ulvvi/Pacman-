9094710f7b015a79460e1c52857ca6fef388b487
Ulvv <guilhermebritodemorais06@gmail.com>
2025-11-26 21:57:52 -0300

    refeito musica 1

--- libs/Character/fantasma.h
+++ libs/Character/fantasma.h
@@ -284,7 +284,7 @@ tInimigo moveFantasma(tInimigo fantasma, tMapa mapa, int indice, tJogador pacman
         fantasma.pos=teleportaFantasma(fantasma);
     }
     if((indice%20==0 && (fantasma.pos.x>=40 && fantasma.pos.x<=1520) && (fantasma.pos.y>=40 && fantasma.pos.y<=720)) && !pacman.power_pellet){
-        fantasma.direcao=escolheDirIntercepta(&fantasma, &pacman, &mapa);
+        fantasma.direcao=escolheDirPersegue(&fantasma, &pacman, &mapa);
     }
     if((indice%20==0 && (fantasma.pos.x>=40 && fantasma.pos.x<=1520) && (fantasma.pos.y>=40 && fantasma.pos.y<=720)) && pacman.power_pellet){
         fantasma.direcao=fogePacman(fantasma, mapa.grid_mapa, pacman);
